07bd324d0674daa23ef5b1f834a64a392533cdb1
Anders Sandström <anders.sandstroem@psi.ch>
2026-02-02 15:05:46 +0100

    Allow no axes groups in finalize.cmd

--- scripts/finalize.cmd
+++ scripts/finalize.cmd
@@ -22,10 +22,12 @@ epicsEnvUnset(ECMC_EC_APPLY_CFG_DONE)
 
 # Load axes group info
 ecmcConfig "GetAxisGroupCount()"
-epicsEnvSet(ECMC_AXES_GRP_COUNT,${ECMC_CONFIG_RETURN_VAL=0})
-dbLoadRecords(ecmcAxisGroups.db,"P=${IOC}:,COUNT=${ECMC_AXES_GRP_COUNT=0}")
-ecmcEpicsEnvSetCalc(ECMC_AXES_GRP_LOOP_END,"${ECMC_AXES_GRP_COUNT=0}-1")
-ecmcForLoop(${ecmccfg_DIR}finalizeAxGrp_loopStep.cmd,"P=${IOC}:",ECMC_LOOP_IDX,0,${ECMC_AXES_GRP_LOOP_END=0},1)
+ecmcEpicsEnvSetCalcTernary(ECMC_BLOCK_EXE, "'Error' in '${ECMC_CONFIG_RETURN_VAL}'", "#-", "")
+${ECMC_BLOCK_EXE=}epicsEnvSet(ECMC_AXES_GRP_COUNT,${ECMC_CONFIG_RETURN_VAL=0})
+${ECMC_BLOCK_EXE=}dbLoadRecords(ecmcAxisGroups.db,"P=${IOC}:,COUNT=${ECMC_AXES_GRP_COUNT=0}")
+${ECMC_BLOCK_EXE=}ecmcEpicsEnvSetCalc(ECMC_AXES_GRP_LOOP_END,"${ECMC_AXES_GRP_COUNT=0}-1")
+${ECMC_BLOCK_EXE=}ecmcForLoop(${ecmccfg_DIR}finalizeAxGrp_loopStep.cmd,"P=${IOC}:",ECMC_LOOP_IDX,0,${ECMC_AXES_GRP_LOOP_END=0},1)
+epicsEnvUnset(ECMC_BLOCK_EXE)
 
 #- Set app mode if not done
 ecmcIf("${ECMC_SET_APP_MODE_DONE=0}!=1")
